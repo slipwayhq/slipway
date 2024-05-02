48e23cd0fbabcba0513da1d0aae73e6320982693
James Thurley <james@thurley.com>
2024-05-02 19:49:22 +0100

    Update usings

--- src/slipway-lib/src/execute/validate_component_io.rs
+++ src/slipway-lib/src/execute/validate_component_io.rs
@@ -1,6 +1,6 @@
 use tracing::warn;
 
-use crate::{errors::AppError, AppSession, ComponentState};
+use crate::{errors::AppError, AppSession, ComponentLoaderErrorBehavior, ComponentState};
 
 pub(super) fn validate_component_io<'app>(
     session: &'app AppSession,
@@ -12,16 +12,17 @@ pub(super) fn validate_component_io<'app>(
     let maybe_component_definition = component_cache.get_definition(component_reference);
     match &maybe_component_definition.value {
         Some(component_definition) => {
+            // If the component was loaded but there were loader failures, either error or warn
+            // depending on the session options.
             if !maybe_component_definition.loader_failures.is_empty() {
                 match session.component_load_error_behavior {
-                    crate::execute::app_session::ComponentLoaderErrorBehavior::ErrorAlways => {
+                    ComponentLoaderErrorBehavior::ErrorAlways => {
                         return Err(AppError::ComponentLoadFailed(
                             component_state.handle.clone(),
                             maybe_component_definition.loader_failures.clone(),
                         ));
                     }
-                    crate::execute::app_session::ComponentLoaderErrorBehavior::ErrorIfComponentNotLoaded => {
-                        // Log that errors were reported by loaders despite the component being loaded.
+                    ComponentLoaderErrorBehavior::ErrorIfComponentNotLoaded => {
                         for loader_failure in &maybe_component_definition.loader_failures {
                             warn!(
                                 "component {} was loaded but an earlier loader {} reported an error: {}",
@@ -36,6 +37,8 @@ pub(super) fn validate_component_io<'app>(
                     }
                 }
             }
+
+            // Validate the data against either the component input or output schema.
             match validation_data {
                 ValidationData::Input(input) => {
                     jtd::validate(&component_definition.input, input, Default::default()).map_err(
