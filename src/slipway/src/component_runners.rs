b9e20cfe054f36870272f16a83f69878a541db18
James Thurley <james@thurley.com>
2024-12-04 19:22:54 +0000

    Include all component runners in cli

--- src/slipway/src/component_runners.rs
+++ src/slipway/src/component_runners.rs
@@ -1,5 +1,9 @@
 use slipway_engine::ComponentRunner;
 
 pub fn get_component_runners() -> Vec<Box<dyn ComponentRunner>> {
-    vec![Box::new(slipway_wasmtime_runner::WasmComponentRunner {})]
+    vec![
+        Box::new(slipway_engine::SpecialComponentRunner {}),
+        Box::new(slipway_fragment_runner::FragmentComponentRunner {}),
+        Box::new(slipway_wasmtime_runner::WasmComponentRunner {}),
+    ]
 }
