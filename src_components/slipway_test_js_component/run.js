dd74c06c33c1120eca42cf0583d45f74ecd85116
James Thurley <james@thurley.com>
2024-12-11 17:51:46 +0000

    Add throw command for js_test component

--- src_components/slipway_test_js_component/run.js
+++ src_components/slipway_test_js_component/run.js
@@ -1,11 +1,26 @@
-let result = {
-  value: input.value + 1
-};
+run();
 
-console.trace("This is trace");
-console.debug("This is debug");
-console.log("This is info");
-console.warn("This is warn");
-console.error("This is error");
+function run() {
+  switch (input.type) {
+    case "increment":
+      console.error("This is an error.");
+      console.warn("This is a warning.");
+      console.log("This is information.");
+      console.debug("This is debug information.");
+      console.trace("This is trace information.");
+      return {
+        value: input.value + 1
+      };
+    
+      case "throw":
+        throw new Error("This is a thrown error.");
 
-result;
+      case "read_file":
+        const data = Deno.readTextFileSync("hello.txt");
+        console.log(data);
+        break;
+
+      default:
+          throw new Error("Unexpected input type: " + input.type);
+  }  
+}
