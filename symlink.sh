fc9d1f7cda39956923d25c52acc2e96447d93928
James Thurley <james@thurley.com>
2025-03-10 20:03:35 +0000

    Update symlinks so release is the default

--- symlink.sh
+++ symlink.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ln -sf $(pwd)/src/target/debug/slipway ~/bin/slipway
-ln -sf $(pwd)/src/target/release/slipway ~/bin/slipwayr
+ln -sf $(pwd)/src/target/debug/slipway ~/bin/slipwayd
+ln -sf $(pwd)/src/target/release/slipway ~/bin/slipway
