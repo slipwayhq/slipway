e08235836fdb2598a9510d68999ebb8cc66f46bd
James Thurley <james@thurley.com>
2025-03-12 08:50:10 +0000

    Handle ID being sent with trmnl log request

--- src/slipway/src/serve/trmnl/log.rs
+++ src/slipway/src/serve/trmnl/log.rs
@@ -1,7 +1,10 @@
 use actix_web::{post, web, HttpRequest, Responder};
 use tracing::{info, instrument};
 
-use crate::serve::{trmnl::get_api_key_from_headers, ServeError, ServeState};
+use crate::serve::{
+    trmnl::{authenticate_device, get_api_key_from_headers, get_device_id_from_headers},
+    ServeError, ServeState,
+};
 
 #[post("/log")]
 #[instrument(name = "trmnl_log", skip_all)]
@@ -15,13 +18,20 @@ pub(crate) async fn trmnl_log(
         Err(e) => format!("Failed to parse log text as UTF-8\n{e}"),
     };
 
-    // Firmware doesn't send ID with log messages, so we can't look up the device by ID.
-    // let id = get_device_id_from_headers(&req)?;
-    // let (device_name, device) = data.repository.get_device_by_id(id).await?;
-    // authenticate_device(id, &req, &device)?;
-
-    let api_key = get_api_key_from_headers(&req)?;
-    let (device_name, _device) = data.repository.get_device_by_api_key(api_key).await?;
+    let id = get_device_id_from_headers(&req);
+    let device_name = match id {
+        Ok(id) => {
+            let (device_name, device) = data.repository.get_device_by_id(id).await?;
+            authenticate_device(id, &req, &device)?;
+            device_name
+        }
+        Err(_e) => {
+            // Older TRMNL firmware doesn't supply the device ID in the log headers.
+            let api_key = get_api_key_from_headers(&req)?;
+            let (device_name, _device) = data.repository.get_device_by_api_key(api_key).await?;
+            device_name
+        }
+    };
 
     info!("Log from device \"{device_name}\": {log_text}",);
 
